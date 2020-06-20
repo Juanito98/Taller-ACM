8fdab4a473744d64508c21cfdeb43872d5ef5007
Juan Carlos Sigler Priego <jcsiglerp@MacBook-Pro-de-Juan.local>
2020-06-19 21:28:21 -0500

    Ordenar en C++

---
+++ Ordenar.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct par {
+    int inicio;
+    int fin;
+};
+
+// Sobrecargamos el operador < para que sort sepa como ordenar
+bool operator<(par& a, par& b) {
+    if (a.inicio < b.inicio) return true;
+    if (a.inicio > b.inicio) return false;
+    if (a.fin < b.fin) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    // your code goes here
+    int n = 6;
+
+    // Inicializamos un arreglo de pares
+    par arr[] = {{2, 3}, {3, 4}, {3, 4}, {-1, 2}, {9, 8}, {8, 7}};
+
+    sort(arr, arr + n);  // Nlog N
+
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i].inicio << " " << arr[i].fin << endl;
+    }
+    cout << endl;
+    return 0;
+}
